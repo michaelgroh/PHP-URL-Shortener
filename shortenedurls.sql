19cfdf416772034ad2514cb00ccb940bb99b47d6
Michael Groh <mike@mikegroh.net>
2013-06-27 12:50:21 -0700

    Update shortenedurls.sql
    Added url field with an eye towards having custom URLs and an example for starting URLs at a desired length.

--- shortenedurls.sql
+++ shortenedurls.sql
@@ -7,8 +7,22 @@ CREATE TABLE `shortenedurls` (
   `long_url` varchar(255) NOT NULL,
   `created` int(10) unsigned NOT NULL,
   `creator` char(15) NOT NULL,
+  `url` varchar(255) NOT NULL,
   `referrals` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`),
   UNIQUE KEY `long` (`long_url`),
+  UNIQUE KEY `url` (`url`),
   KEY `referrals` (`referrals`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Change starting URL length (optional)
+--
+
+-- If you want to start your URLs at a certain length
+-- update the auto increment to x^(y-1) where x is the
+-- number of allowed chars and y is the desired 
+-- starting length of your URLs.
+
+-- 3 character URLS (31^2 = 961)
+ALTER TABLE `shortenedurls` AUTO_INCREMENT = 961;
